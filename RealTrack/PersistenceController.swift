f30db1c8ab5dd035a8c76ec9f409ec9c94f61afb
Robbie Williams <jarobusa1@gmail.com>
2025-03-27 21:18:43 -0400

    #REA-6 remove extra appicons

--- RealTrack/PersistenceController.swift
+++ RealTrack/PersistenceController.swift
@@ -14,12 +14,15 @@ struct PersistenceController {
     let container: ModelContainer
 
     init(inMemory: Bool = false) {
-        // Define the schema for your SwiftData models
+        // Define the SwiftData schema by listing all model types used in the app.
+        // This schema is passed into the configuration to inform the ModelContainer
+        // of the structure of your data models.
         let schema = Schema([
             AddressModel.self,
             PersonModel.self,
             PersonTypeModel.self,
-            AddressTypeModel.self 
+            AddressTypeModel.self,
+            OrganizationModel.self
         ])
         // Create a configuration with an option to store in memory (for testing) or on disk
         let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: inMemory)
